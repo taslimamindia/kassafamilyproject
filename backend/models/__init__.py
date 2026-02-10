b6b5890fe5c7767664bc2b82ed0f111783541520
Mamadou Taslima Diallo <mamadou.taslima.diallo@usherbrooke.ca>
2026-02-10 02:46:58 -0500

    refactor: remove deprecated database and model files

--- backend/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .users import Users, FamilyAssignation
-from .access_control import Roles, RoleAttribution
-from .finance import PaymentMethods, Transactions, TransactionApprovals
-from .messaging import Messages, MessageRecipients
